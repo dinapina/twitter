310a74ba9c1e9cae1caa3ba411791baf0053f0eb
Dina Pina <dina.mukasheva@gmail.com>
2014-12-10 09:21:10 +0000

    9th chapter

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.1.0'
+gem 'bootstrap-sass', '3.2.0.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 group :development do
   gem 'sqlite3', '1.3.8'
@@ -14,6 +16,9 @@ gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   
+  
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
   match '/help', to: 'static_pages#help', via: :get
   match '/about', to: 'static_pages#about', via: :get
   match '/contact', to: 'static_pages#contact', via: :get
-  
-  get 'static_pages/home'
-  
+  match '/signup', to: 'users#new', via: :get
+  match '/signin', to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
+  # instead of get 'static_pages/home'
+  #get 'users/new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
